8d9f1646b3e0e6bbea8ffe394513e6e46a79fa1c
ratsilva <ricardoatsilva1@gmail.com>
2025-01-12 23:33:38 -0300

    solution 1

--- app/src/main/java/com/ricardo/workshop/android/ui/MainActivity.kt
+++ app/src/main/java/com/ricardo/workshop/android/ui/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.ricardo.workshop.android.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.ricardo.workshop.android.R
+import com.ricardo.workshop.android.data.LocalDataSource
 
 internal class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // attach fragment to activity
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, ToastListFragment.newInstance())
-            .commitNow()
+        setContent {
+            ToastListScreen(toasts = LocalDataSource().getToasts())
+        }
     }
 }
